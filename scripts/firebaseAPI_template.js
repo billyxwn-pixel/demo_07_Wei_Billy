4dd0557f1f480833f29130cca962a50299dc6c57
billyxwn-pixel <77774076+billyxwn-pixel@users.noreply.github.com>
2023-10-31 18:48:56 -0700

    Demo09 Add-ons

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,17 +2,19 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+
+    apiKey: "AIzaSyCdgruwSiiQV9qLmaMm-lLAMLAcrjRgqWE",
+    authDomain: "demo07-91471.firebaseapp.com",
+    projectId: "demo07-91471",
+    storageBucket: "demo07-91471.appspot.com",
+    messagingSenderId: "938237383085",
+    appId: "1:938237383085:web:73d18222530f1bcb59d124",
+    measurementId: "G-N4EW25P39C"
 };
 
 //--------------------------------------------
-// initialize the Firebase app
+// initialize the Firebase app that's aboce
 // initialize Firestore database if using it
 //--------------------------------------------
 const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const db = firebase.firestore(); //creates a new database for us, "db"
